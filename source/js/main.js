6b91435ca7572b1203facfa89f729e2279fa7a22
Zdravoles <zdravoles@gmail.com>
2022-05-12 12:23:51 +0300

    модальное окно в корзину

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,6 @@
 import './menu.js';
+import {showPopup} from './popup.js';
+
+const nodeModalButton = document.querySelector('.jsModalButton');
+
+nodeModalButton.addEventListener('click', () => showPopup('order'));
