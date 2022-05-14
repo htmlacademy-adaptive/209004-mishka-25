518d238349d35b4744f55ea6b38da496a018eecd
Zdravoles <zdravoles@gmail.com>
2022-05-14 20:26:55 +0300

    правки от наставника

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,7 @@ import './menu.js';
 import {showPopup} from './popup.js';
 
 if (document.querySelector('#addBasketModal')) {
-  const arrModalButtons = document.querySelectorAll('.jsModalButton');
+  const arrModalButtons = document.querySelectorAll('#jsModalButton');
 
   for (let nodeModalButton of arrModalButtons) {
     nodeModalButton.addEventListener('click', () => showPopup('order'));
