1b6784882442e0f2fca36f5d22eb027668cf1568
Zdravoles <zdravoles@gmail.com>
2022-05-17 21:25:53 +0300

    Автоматизация сборки личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,14 @@ import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import svgo from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import rename from 'gulp-rename';
+import del from 'del';
+import csso from 'postcss-csso';
+import squoosh from 'gulp-libsquoosh';
+
+const BUILD_FOLDER = 'build';
 
 // Styles
 
@@ -16,6 +24,71 @@ export const styles = () => {
     ]))
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
+}
+
+// del build folder
+const delfolder = () => {
+  return del(BUILD_FOLDER);
+}
+
+// copying files
+export const copyfiles = (done) => {
+  return gulp.src(
+    [
+      "source/fonts/*.{woff,woff2}",
+      "source/*.ico",
+      "source/manifest.webmanifest",
+      "source/*.html"
+    ],
+    {
+      base: "source"
+    }
+  )
+  .pipe(gulp.dest(BUILD_FOLDER))
+  done();
+}
+
+// sprite
+export const sprite = () => {
+  return gulp.src('source/img/icons-for-sprite/*.svg')
+    .pipe(svgo())
+    .pipe(svgstore())
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest(BUILD_FOLDER+'/img/'));
+}
+
+// minify SVG
+export const minifySvg = () => {
+  return gulp.src(['source/img/*.svg', '!source/img/sprite.svg'])
+  .pipe(svgo())
+  .pipe(gulp.dest(BUILD_FOLDER+'/img/'));
+}
+
+// minify Styles
+const minifyStyles = () => {
+  return gulp.src('source/css/*.css', { sourcemaps: true })
+  .pipe(postcss([
+    autoprefixer(),
+    csso()
+  ]))
+  .pipe(rename('style.min.css'))
+  .pipe(gulp.dest(BUILD_FOLDER+'/css/', { sourcemaps: '.' }));
+}
+
+// images optimization
+export const imagesOptimization = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(squoosh())
+  .pipe(gulp.dest(BUILD_FOLDER+'/img/'));
+}
+
+// Webp optimization
+export const webp = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(squoosh({
+    webp: {},
+  }))
+  .pipe(gulp.dest(BUILD_FOLDER+'/img/'));
 }
 
 // Server
@@ -39,6 +112,22 @@ const watcher = () => {
   gulp.watch('source/*.html').on('change', browser.reload);
 }
 
+export const production = gulp.series(
+  delfolder,
+  copyfiles,
+  gulp.parallel(
+    sprite,
+    minifySvg,
+    minifyStyles,
+    imagesOptimization,
+    webp
+  )
+);
+
+export const start = gulp.series(
+  production,
+  server
+);
 
 export default gulp.series(
   styles, server, watcher
